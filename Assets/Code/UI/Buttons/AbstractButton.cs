e66cb1a26cf18d9cff7206f96d9cc8d277830eae
lolesch <leonid.schreiber@yahoo.de>
2024-05-17 00:30:18 +0200

    [+] ActionButton.cs with click and hover behavior

---
+++ Assets/Code/UI/Buttons/AbstractButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace CodingTest_TF.UI.Buttons
+{
+    public abstract class AbstractButton : Selectable, IPointerClickHandler
+    {
+        // TODO: disable the button for x seconds to disable button spaming
+
+        protected abstract void OnLeftClick();
+        protected abstract void OnRightClick();
+
+        public virtual void OnPointerClick(PointerEventData eventData)
+        {
+            if (!interactable)
+                return;
+
+            if (eventData.button == PointerEventData.InputButton.Left)
+                OnLeftClick();
+            else if (eventData.button == PointerEventData.InputButton.Right)
+                OnRightClick();
+        }
+    }
+}
